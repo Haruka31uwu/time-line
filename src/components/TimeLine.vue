84bb05de2805a29e2fa5514e954594da0a2e7564
francis <francis.torres@alarcongroup.us>
2023-09-11 11:53:05 -0500

    0w0

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -37,119 +37,19 @@ import TimeLineItem from './TimeLineItem.vue'
 import ModalMuf from './ModalMuf.vue'
 
 export default {
+    props:{
+        timelineItems:{
+            type:Object,
+            required:false,
+            default:()=>({})
+        }
+    },
     components:{
         TimeLineItem,
         ModalMuf
     },
     data(){
         return {
-            timelineItems:{
-                jobTitleInfo:{
-                    jobTitle:"Frontend Developer",
-                    department:"Paragon",
-                    mufId:"MUF-123",
-                    dateStart:"01/01/2020",
-                },
-                stages:[
-                    {
-                        name:"CV Evaluation",
-                        candidates:[
-                            {
-                              idCandidate:1,
-                              name:"Francis Torres"
-                            },
-                            {
-                              idCandidate:2,
-                              name:"Abraham Flores"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Assesment In Progress",
-                        candidates:[
-                            {
-                              idCandidate:3,
-                              name:"Jairo Espinoza"
-                            },
-                            {
-                              idCandidate:4,
-                              name:"Bill Morales"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Presencial Interview",
-                        candidates:[
-                            {
-                              idCandidate:5,
-                              name:"Roger Anyosa"
-                            },
-                            {
-                              idCandidate:6,
-                              name:"Paulo Ugarte"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Psychological Test",
-                        candidates:[
-                            {
-                              idCandidate:7,
-                              name:"Anderson Vallejo"
-                            },
-                            {
-                              idCandidate:8,
-                              name:"Kevin Paredes"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Technical Interview",
-                        candidates:[
-                            {
-                              idCandidate:9,
-                              name:"Christian CasaFranca"
-                            },
-                            {
-                              idCandidate:10,
-                              name:"Ommar Paredes"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Pending Chief Approval",
-                        candidates:[
-                            {
-                              idCandidate:11,
-                              name:"Brayan Mendoza"
-                            },
-                            {
-                              idCandidate:12,
-                              name:"Jhon Landeo"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Approved By Chief",
-                        candidates:[
-                            {
-                              idCandidate:13,
-                              name:"Carolina Villegas"
-                            },
-                        ]
-                    },
-                    {
-                        name:"Rejected By Ceo",
-                        candidates:[
-                            {
-                              idCandidate:14,
-                              name:"Brigitte Mendez"
-                            },
-                        ]
-                    }
-                ],
-                
-            },
             showInfoMuf:false,
             suggestOptions:[{
               name:'Francis Torres',
